ebe9a0983c67b32aa2ac32ad075b14572f7b4fc0
“Foxshane997” <Foxshane997@gmail.com>
2024-07-01 00:16:50 -0700

    Added all changes needed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
+  let [counter, changeCounter] = useState(1);
+  window.changeCounter = changeCounter;
+
+  const handleIncrement = () => {
+    changeCounter(counter + 1);
+  };
+
+  const handleDecrement = () => {
+    changeCounter(counter - 1);
+  };
+
+  useEffect(() => {
+    document.title = "Counter App";
+
+    return () => {
+      document.title = "Counter App";
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="counter-container">
+        <h1 className="header">The Current Number is</h1>
+        <h2 className="header">{counter}</h2>
+
+        <button className="button-increment" onClick={handleIncrement}>
+          +1
+        </button>
+        <button
+          className="button-decrement"
+          onClick={handleDecrement}
+          disabled={counter <= 0}
         >
-          Learn React
-        </a>
-      </header>
+          -1
+        </button>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../App.css";
+
+function Footer() {
+  return (
+    <footer>
+      <a href="https://github.com/Foxshane997" target="blank">
+        GitHub
+      </a>
+      <span></span>
+      <a href="https://www.linkedin.com/in/shane-fox-19ba382a9/" target="blank">
+        LinkedIn
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
